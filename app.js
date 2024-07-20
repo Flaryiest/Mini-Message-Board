ca8ff34e300a9850d17ad95ef495a8698c478334
Flaryiest <ericmzuo1@gmail.com>
2024-07-20 13:30:58 -0600

    created basic mini message board no styling

---
+++ app.js
@@ -0,0 +1,38 @@
+express = require('express')
+path = require('path')
+app = express()
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", 'ejs')
+
+app.use(express.urlencoded({extended: true}))
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ]; 
+
+app.get('/', (req, res, next) => {
+     
+    res.render("index", {messages: messages})
+})
+
+app.get('/new', (req, res, next) => {
+    res.render("form")
+})
+
+app.post('/new', (req, res) => {
+    console.log(req.body)
+    messages.push({text: req.body.text, user: req.body.user, added: new Date() })
+    res.redirect("/")
+})
+
+const PORT = 3000
+app.listen(PORT, () => console.log("working!"))
